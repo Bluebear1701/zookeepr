052af192ab5b5f9b813d81a58a5f1ee89f94befd
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-11 00:26:23 -0500

    filterByQuery is returning undefined

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ const express = require("express");
 const app = express();
 const { animals } = require('./data/animals');
 
-app.get("/api/animals", (req, res) => {
-    res.json(animals);
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+  }
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
   });
   
 app.listen(3001, () => {
